a896841e95dde77bba59b7c807ab6d265290c37b
Michael Hoffert <mike.hoffert@gmail.com>
2015-05-04 23:43:44 -0600

    Update asset_file.rb

--- lib/inline_svg/asset_file.rb
+++ lib/inline_svg/asset_file.rb
@@ -6,7 +6,7 @@ module InlineSvg
     def self.named(filename)
       asset_path = FindsAssetPaths.by_filename(filename) rescue nil
       asset_path = FindsPublicPaths.by_filename(filename) if asset_path.nil? rescue nil
-      raise FileNotFound.new("Asset not found: #{asset_path}") if asset_path.nil?
+      asset_path = filename
       File.read(asset_path || UNREADABLE_PATH)
     end
   end
