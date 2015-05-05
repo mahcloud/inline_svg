51377e5bfcd250c5d7af89c25e3764df41596530
Michael Hoffert <mike.hoffert@gmail.com>
2015-05-04 22:55:30 -0600

    Accept asset paths from public directory
    Using paperclip saves svg files in public directory.
    It would be nice if this gem would allow these files.

--- lib/inline_svg/asset_file.rb
+++ lib/inline_svg/asset_file.rb
@@ -4,10 +4,10 @@ module InlineSvg
     UNREADABLE_PATH = ''
 
     def self.named(filename)
-      asset_path = FindsAssetPaths.by_filename(filename)
+      asset_path = FindsAssetPaths.by_filename(filename) rescue nil
+      asset_path = FindsPublicPaths.by_filename(filename) if asset_path.nil? rescue nil
+      raise FileNotFound.new("Asset not found: #{asset_path}") if asset_path.nil?
       File.read(asset_path || UNREADABLE_PATH)
-    rescue Errno::ENOENT
-      raise FileNotFound.new("Asset not found: #{asset_path}")
     end
   end
 end
